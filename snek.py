66411b7d4fe1cca10a96939c18867316ba3a5c75
zhandavidz <46409638+zhandavidz@users.noreply.github.com>
2020-10-28 23:20:49 -0700

    generate the window and add the main loop, along with an fps controller

--- snek.py
+++ snek.py
@@ -4,3 +4,32 @@ import pygame
 
 # initialize pygame
 pygame.init()
+
+# set window dimensions
+screen_width = 500
+screen_height = 500
+
+win = pygame.display.set_mode((screen_width, screen_height))
+
+# set the title of the window
+pygame.display.set_caption("Snekbot")
+
+# set the clock used for managing fps in the main loop
+clock = pygame.time.Clock()
+
+
+# main loop
+run = True
+while run:
+    # set the fps
+    clock.tick(30)
+
+    # end the loop when the user closes the window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    # refresh the window
+    pygame.display.update()
+
+pygame.quit()
